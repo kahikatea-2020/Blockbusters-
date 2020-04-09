8cff88ef5e43379c10a2874eb053203110c30d4b
Matthew Kalopulu <matthew_kalopulu@hotmail.com>
2020-04-10 10:24:23 +1200

    more initializing

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+
+//this will be the import of all component
+import Header from './Header'
+import Footer from './Footer'
+import Home from './Home'
+import Form from './Form'
+import Movie from './Movie'
+import Result from './Result'
+
 
 const App = () => {
   return (
-    <h1>React development has begun!</h1>
+    <h1>Coming soon to a Countdown near you!</h1>
   )
 }
 
